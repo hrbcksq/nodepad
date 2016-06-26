984d75dec907ac24aca7905829834b737151f812
hrbcksq <hrbcksq@gmail.com>
2016-06-26 04:03:22 +0300

    go sleep;)

---
+++ jscore/misunderstanding.js
@@ -0,0 +1,12 @@
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE > 0);
+
+// smallest value is closest to zero
+console.log(Number.MIN_VALUE);
+console.log(Number.MIN_VALUE < 0);
+
+// Left and right associative operators
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
+
+
